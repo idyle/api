413fc322c8fd357f3d6d2a27c772b4cec3149d0a
marcus <marcus.imperial01@gmail.com>
2023-08-18 00:58:27 +0800

    minor edit to page config (editor)

--- editor/operations.js
+++ editor/operations.js
@@ -55,8 +55,8 @@ export const convertPageToHtml = (data, metadata, name) => {
         return `
         <html lang="en">
             <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 ${head}
                 ${aosCss}
             </head>
